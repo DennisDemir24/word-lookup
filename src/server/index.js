c46a313e37648034d05ea0a65d14117885671128
Dennis Demir <dd222gc@student.lnu.se>
2024-03-02 10:46:07 +0100

    Adding database connection

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,17 @@
 import express from "express";
+import dotenv from 'dotenv'
+dotenv.config()
+import connectDB from './config/db.js'
 import router from "./routes/routes.js";
 
-const app = express();
 const port = 8080;
 
+connectDB();
+
+const app = express();
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
 app.use("/api", router);
 
 
