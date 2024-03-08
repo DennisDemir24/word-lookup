37ab636649db6175fbbf4abe4096759b4be93e27
Dennis Demir <dd222gc@student.lnu.se>
2024-03-08 07:18:37 +0100

    Adding cors to backend

--- src/server/index.js
+++ src/server/index.js
@@ -12,6 +12,12 @@ connectDB();
 const app = express();
 
 app.use(cors())
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://word-lookup.vercel.app')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  next()
+})
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
