7a8f3e9a8b7adfdc3efb59c56290e2e1589d377d
Dennis Demir <dd222gc@student.lnu.se>
2024-03-09 16:38:16 +0100

    Finishing touches

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,55 +1,32 @@
-/* eslint-disable no-undef */
-import { useState, useEffect } from "react"
-import { getRandomWord } from "../services/api"
-import SearchForm from "../components/SearchForm/SearchForm"
-import Card from "../components/Card/Card"
+import { useState } from 'react'
+import { getRandomWord } from '../services/api'
+import SearchForm from '../components/SearchForm/SearchForm'
+import Card from '../components/Card/Card'
 import './pages.css'
 
 const HomePage = () => {
-    const [searchResults, setSearchResults] = useState([])
-    const [searchMessage, setSearchMessage] = useState('')
-    const [randomWord, setRandomWord] = useState([])
-
-    const callOpenAI = async () => {
-        console.log("OPEN AI")
-        const response = await fetch('https://api.openai.com/v1/completions', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`
-          },
-          body: JSON.stringify({
-            model: 'text-davinci-003',
-            prompt: `Create a sentence with the word ${randomWord.word}`,
-            max_tokens: 1000,
-        })
-      })
-      const data = await response.json()
-      console.log("DATA",data)
-    }
-
-    const handleSearchResults = (results) => {
-      setSearchResults(results)
-      console.log("results",searchResults)
-      setSearchMessage(results.length === 0 ? 'No such word exists in the dictionary. You can add it and its synonyms using the add word button.' : '')
-      callOpenAI()
-    }
-
-    useEffect(() => {
-      const fetchRandomWord = async () => {
-        const word = await getRandomWord()
-        setRandomWord(word)
-      }
-      fetchRandomWord()
-    }, [])
-
-    console.log("random word",randomWord)
+  const [searchResults, setSearchResults] = useState([])
+  const [searchMessage, setSearchMessage] = useState('')
 
 
+  const handleSearchResults = (results) => {
+    setSearchResults(results)
+    setSearchMessage(
+      results.length === 0
+        ? 'No such word exists in the dictionary. You can add it and its synonyms using the add word button.'
+        : ''
+    )
+  }
+
+  const onGenerateRandomWord = async () => {
+    const randomWord = await getRandomWord()
+    setSearchResults([randomWord])
+    setSearchMessage('')
+  }
 
   return (
     <>
-      <section className="home-section">
+      <section className='home-section'>
         <div>
           <div>
             <h1
@@ -60,27 +37,38 @@ const HomePage = () => {
               Search for a word
             </h1>
           </div>
-          <div className="form-wrapper">
+          <div className='form-wrapper'>
             <SearchForm onSearchResults={handleSearchResults} />
+            <p>Or</p>
+            <button
+              onClick={onGenerateRandomWord}
+              className='add-button'
+              style={{ backgroundColor: '#593C8F', color: '#fff' }}
+            >
+              Generate random Word
+            </button>
           </div>
           {searchResults.length === 0 && (
-            <p style={{ color: '#fff', textAlign: 'center' }}>{searchMessage}</p>
+            <p style={{ color: '#fff', textAlign: 'center' }}>
+              {searchMessage}
+            </p>
           )}
-          <ul>
-            {searchResults.map((result) => (
-              <>
-                <Card key={result.word}>
-                  <h3>{result.word}</h3>
-                  <p>Synonyms:</p>
-                  <div className="tag-wrapper">
-                    {result.synonyms.map((synonym) => (
-                      <span className="synonym-tag" key={synonym}>{synonym}</span>
-                    ))}
-                  </div>
-                </Card>
-              </>
-            ))}
-          </ul>
+
+          {searchResults.map((result) => (
+            <>
+              <Card key={result.word}>
+                <h3 className='word-title'>{result.word}</h3>
+                <p className='synonym-title'>Synonyms:</p>
+                <div className='tag-wrapper'>
+                  {result.synonyms.map((synonym) => (
+                    <span className='synonym-tag' key={synonym}>
+                      {synonym}
+                    </span>
+                  ))}
+                </div>
+              </Card>
+            </>
+          ))}
         </div>
       </section>
     </>
